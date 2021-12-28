6e5fb8b03e82d631abbabbcfd77dbea9bdb7cab6
Sam Wells <samwells95@gmail.com>
2021-12-27 23:47:38 -0800

    init

---
+++ src/models/user.ts
@@ -0,0 +1,11 @@
+export class User {
+    firstName: string
+    surname: string
+    age: number
+
+    constructor(firstName: string, surname: string, age: number) {
+        this.firstName = firstName;
+        this.surname = surname;
+        this.age = age;
+    }
+}
